4cf6e86f313be5ba5e1d1cb6e2d5485738c8fd8a
DongheonPark <5623143@stu.kmu.ac.kr>
2025-03-11 12:57:02 +0900

    테스트

--- 1완성_박동헌_5623143_0311.c
+++ 1완성_박동헌_5623143_0311.c
@@ -3,10 +3,10 @@
 int main() {
     int num;
 
-    printf("정수를 입력하세요: ");
+    printf("?????? ????????: ");
     scanf("%d", &num);
 
-    printf("입력된 정수의 32비트 표현: ");
+    printf("???? ?????? 32??? ???: ");
     for (int i = 31; i >= 0; i--) {
         int change = num >> i & 1;
         printf("%d", change);
